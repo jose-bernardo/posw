17688ea2596112def6db4dffc097eb558712d3e4
ist195651 <nuno.policarpo@tecnico.ulisboa.pt>
2023-05-18 16:33:02 +0100

    iter converted to ints

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 from Crypto.Random import get_random_bytes
 from hashlib import sha256
-from math import log2
+
+import json as mnel
+
 
 """
 N The time parameter which we assume is of the form N = 2n+1 − 1 for
@@ -27,8 +29,8 @@ def H(b: bytes) -> bytes:
     return m.digest() 
 
 def main():
-    #n = int(input("depth n: "))
-    n = 4
+    n = int(input("depth n: "))
+    #n = 4
     #print("n:", n)
 
     #t = int(input("security t: "))
@@ -40,8 +42,23 @@ def main():
 
     #N = (1 << n + 1) - 1
 
-    h = compute_hash(n, 1, [])
-    print(h)
+    # h = compute_hash(n, 1, [])
+    # print(h)
+    #node = 0b100
+    #size = 2
+    #print_node(node)
+    #node, size = next_node(node, size, n)
+    #print_node(node)
+    #node, size = next_node(node, size, n)
+    #print_node(node)
+    #node, size = next_node(node, size, n)
+    #print_node(node)
+    #print_node(node)
+    #node, size = next_node(node, size, n)
+    #print_node(node)
+
+    compute(n, printer)
+    print (mnel.dumps(debug_tree, indent=2))
 
 def compute_hash(n: int, id: int, leaves):
     if n < 0:
@@ -61,6 +78,69 @@ def compute_hash(n: int, id: int, leaves):
         s += b''.join(list(map(H, map(bytes, leaves))))
 
     return H(s)
+
+def printer(labels):
+    return "|".join(map(lambda id : str(bin(id)[3:]), labels))
+
+def str_node(node : int):
+    return str(bin(node))[3:]
+
+def next_node(id : int, size : int, n : int):
+    
+    # 0001 -> 000
+
+    # size = 1
+    # n = 4
+
+    # 0 -> 1000
+    # 0 + 1 = 1
+    # 1000
+
+    # 1 << size ^ id
+
+    # id -> 00000
+    # id -> 0
+
+    #print_node(id)
+    #print_node(id + 1)
+    #print(f"{id=} {n=} {size=}")
+    #print_node(id + 1)
+    #print()
+    if id % 2 == 1:
+        return (id >> 1, size - 1)
+    else:
+        return ((id + 1) << (n - size), n)
+    #(id[:-1] + "1").ljust(n, '0')
+        #return id[:-1] + "1" + "0" * (n - len(id))
+
+def compute(n, hash_function = H):
+    id = 1 << n
+    size = n
+
+    parent_labels = []
+    
+    
+    while (size != 0):
+        print(id)
+        print(f"{parent_labels= }")
+
+        if (size < n):
+            debug_tree[str_node(id)] = hash_function([id] + parent_labels[-2:])
+        else:
+            debug_tree[str_node(id)] = hash_function([id] + parent_labels)
+
+        if (size < n):
+            parent_labels.pop()
+            parent_labels.pop()
+
+        parent_labels.append(id)
+
+        print(f"og node: {id=} {size=}")
+        id, size = next_node(id, size, n)
+        print(f"next node: {id=} {size=}")
+
+
+debug_tree = {}
 
 if __name__ == "__main__":
     main()
