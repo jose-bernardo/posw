7bd329a6b493144ba6c44b137060d94416292b91
ist195651 <nuno.policarpo@tecnico.ulisboa.pt>
2023-05-22 18:33:08 +0100

    open done, cleanup needed

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 from hashlib import sha256
+from typing import Callable
 import json as mnel
 
 from Crypto.Random import get_random_bytes
 from Crypto.Random.random import randint
 
+RandomOracleType = Callable[[bytes, str, list[str]], str]
 
+#ROOT = ""
+ROOT = "0b1"
 
 # class Node:
 #     def __init__(self, value : int, size : int):
@@ -27,8 +31,6 @@ from Crypto.Random.random import randint
     
 #     def __mod__(self, n : int):
 #         return self.value - n
-
-    
 
 """
 N The time parameter which we assume is of the form N = 2n+1 − 1 for
@@ -51,14 +53,14 @@ def sha256H(chi: bytes, node : str, labels : list[str]) -> str:
     m.update(node.encode('utf-8'))
     m.update("".join(labels).encode('utf-8'))
 
-    return m.hexdigest() 
+    return m.hexdigest()
 
 def printer(chi : bytes, node : str, labels : list[str]) -> str:
     string = node + "|" if labels else node
     return string + "|".join([label.split("|", 1)[0] for label in labels])
 
 def str_node(node : int) -> str:
-    return str(bin(node))[3:]
+    return str(bin(node)) #[3:]
 
 def next_node(id : int, size : int, n : int) -> tuple:
 
@@ -67,7 +69,7 @@ def next_node(id : int, size : int, n : int) -> tuple:
     else:
         return ((id + 1) << (n - size), n)
 
-def posw(n : int, m : int, chi : bytes, H=sha256H) -> dict[str, str]:
+def posw(chi: bytes, n : int, m : int, H: RandomOracleType) -> dict[str, str]:
     tree = {}
     id = 1 << n
     size = n
@@ -85,7 +87,14 @@ def posw(n : int, m : int, chi : bytes, H=sha256H) -> dict[str, str]:
 
         if size <= m : 
             tree[str_node(id)] = label 
+        
+        tree[str_node(id)] = label # TODO TMP
 
+        if id == 0b101:
+            print(f"\n VAI DAR AQUI BARRACA {str_node(id)}\n label: {label}\n\
+                  {label_stack = }\n\
+                  {chi = } \n")
+        
         if size < n:
             label_stack.pop()
             label_stack.pop()
@@ -106,62 +115,56 @@ def generate_challenge(N : int, n :int, t : int) -> list[int]:
     return list(challenge)
     #return sample(range(0, N), t)
 
-#
-# 
-# labels necessarios para folha n: 
-# n ^ 1
-# guardar
-# n >> 1 
-# repetir
-#
-# 0000 -> 0001 
-# 01
-# 0100
-
-# while n != £
-#     xor
-#     guarda
-#     shift
-
-# 0000 -> precisa destes todos: 0001, 001, 01, 1
-# 0001 -> 0000, 001, 01, 1
-
-# 0010 -> rpecisa -> 0011, 000, 01, 1
-
-# se o nó pertence ao set(001, 001, 01, 1, 0011, 000, 01, 1)
-# se sim, entao guarda a hash para enviarmos depois
-# se for uma caca
-# compute a arvore: if label in needed_labels, guarda sff.
-
-def outerspace_posw(n : int, m : int, chi : bytes, tree : dict[str, str], H=sha256H) -> dict[str, str]:
-    id = 1 << m
+def outerspace_posw(chi : bytes, n : int, tree : dict[str, str], initial_stack: list[str], needed_labels : set[int], leaf : int, H: RandomOracleType) -> dict[str, str]:
+    id = leaf
     size = n
 
+    labels = {}
     #id >> (n - size) + 1
-    label_stack = []
+    label_stack = initial_stack
 
-    while (size >= 0):
-        # print(str_node(id))
-        # print(f"{label_stack= }")
+    while (needed_labels):
+        
+        # print(f"current node: {str_node(id)}")
+        # print(f"{label_stack = }")
+        # print(f"needed labels: {list(map(str_node, needed_labels))}")
 
         if (size < n):
             label = H(chi, str_node(id), label_stack[-2:])
+            # label_stack.pop()
+            # label_stack.pop()
         else:
             label = H(chi, str_node(id), label_stack)
 
+ 
+
+        if str_node(id) in tree:
+            print(f"\nBARRACA TOTAL {str_node(id)}\n prev: {tree[str_node(id)]} \nnew: {label}\n\
+                  {label_stack = }\n \
+                  {chi = }\n")
+        
         if size < n:
             label_stack.pop()
             label_stack.pop()
 
         label_stack.append(label)
 
+        
+        
+        tree[str_node(id)] = label
+
+        if id in needed_labels:
+            tree[str_node(id)] = label
+            needed_labels.remove(id)
 #        print(f"og node: {id=} {size=}")
         id, size = next_node(id, size, n)
 #        print(f"next node: {id=} {size=}")
+    
+    # print("Tree: " + mnel.dumps(tree, indent=2))
 
     return tree
 
-def open_nodes(tree: dict[str, str], challenge: list[int], n : int, m : int) -> list[tuple[str,dict[str, str]]]:
+def open_nodes(chi: bytes, n : int , m : int, tree: dict[str, str], challenge: list[int], H: RandomOracleType) -> list[tuple[str,dict[str, str]]]:
 
     needed_labels = [set() for _ in range(1 << m)]
     dic = {}
@@ -177,22 +180,50 @@ def open_nodes(tree: dict[str, str], challenge: list[int], n : int, m : int) -> 
         dic[og_id] = [id]       
 
         #bucket_id = (id ^ (1 << (n))) // (1 << m)
-        print(bucket_id)
+        # print(bucket_id)
+        needed_labels[bucket_id].add(id)
         for i in range(n):
             id ^= 1
             if i <  (n - m):
-                print(str_node(id))
+                # print(str_node(id))
                 needed_labels[bucket_id].add(id)
                 #needed_labels[((id ^ (1 << (n - i))) >> (m - i))].add(id)
             dic[og_id].append(id)
             
             id = id >> 1
+            
+    print(f"path nodes: {dic}")
     
-    print(dic)
-    for bucket in needed_labels:
-        print(list(map(str_node, bucket)))
+    initial_leaf = (1 << n)
+    
+    for i in range(len(needed_labels)):
+        
+        parent_node = i + (1 << m)
+        initial_stack = []
 
-    # m = n case only
+        # if parent_node % 2 == 1:
+        #     parent_node ^= 1
+        #     initial_stack.append(tree[str_node(parent_node)])
+        
+        while(parent_node > 1): # not the empty number
+            if parent_node % 2 == 1:
+                parent_node ^= 1
+                print(f"parent_node era impar: {str_node(parent_node)}")
+                initial_stack.insert(0, tree[str_node(parent_node)])
+
+            
+            parent_node = parent_node >> 1
+        
+        print(f"{i = }\n {initial_stack = }\n \
+              initial leaf = {str_node(initial_leaf)}\n\
+              needed labels ={list(map(str_node, needed_labels[i]))}")
+        
+        outerspace_posw(chi, n, tree, initial_stack, needed_labels[i], initial_leaf, H)
+
+        initial_leaf += 1 << ((n - m))
+    
+    print("Tree: " + mnel.dumps(tree, indent=2))
+
     reply = []
     for id in challenge:
         id = (1 << n ) + id
@@ -207,7 +238,6 @@ def open_nodes(tree: dict[str, str], challenge: list[int], n : int, m : int) -> 
     
     return reply
 
-
 def get_parents(leaf : int, n : int) -> list[str]:
     parents = []
     original_leaf = leaf
@@ -219,10 +249,8 @@ def get_parents(leaf : int, n : int) -> list[str]:
     print(f"Parents of {str_node(original_leaf)} : {parents}")
     return parents
     
-    
-    
 
-def verify(chi : bytes, n : int, root : str, challenges : list[int], reply : list[tuple[str, dict[str, str]]], H=sha256H) -> bool:
+def verify(chi : bytes, n : int, root : str, challenges : list[int], reply : list[tuple[str, dict[str, str]]], H) -> bool:
 
     for i in range(len(challenges)):
         leaf = (1 << n)  + challenges[i]
@@ -239,6 +267,7 @@ def verify(chi : bytes, n : int, root : str, challenges : list[int], reply : lis
         
         if label != H(chi, str_node(leaf), parent_labels):
             print(f"Label of leaf  {str_node(leaf)} is incorrect.")
+            print(f"Expected {label} computed {H(chi, str_node(leaf), parent_labels)}.")
             return False
         
         # verify if root is correct
@@ -253,9 +282,8 @@ def verify(chi : bytes, n : int, root : str, challenges : list[int], reply : lis
             node = node >> 1
             label = H(chi, str_node(node), parents)
 
-            print(f"node = {str_node(node)} {label = }")
+            # print(f"node = {str_node(node)} {label = }")
             
-        
         if label != root:
             print(f"{label = } {root = }")
             print(f"Root is incorrect from leaf {str_node(leaf)}.")
@@ -272,23 +300,23 @@ def main():
 
     w = 256
     chi = get_random_bytes(w//8)
-
+    # chi = b'\xb3\x81\xa5\xaf\xb1\xee$\xb8\xc7JE\xc3"o\x08\xc4\x17(]\'\x07i|*\xb1\x1bB\xf3\xa0\x82\x03\''
+    #print(f"{chi}")
+    #chi = b'"\x94\xd0%\xd1:\xd6\xf9\x9d\xc4B\xf1e\xed\xda\x1bV\x94\xfc\xad=\xf9\xf6\x01\xf7\x07N\x061\x84O\x1b'
     t = int(input("Security parameter t: "))
-
     if ( t > 1 << n):
         print("** Security parameter t lowered to number of leafs 2**n")
         t = 1 << n
         print(f"** Updated t to: {t}", )
 
     m = int(input("Memory tree depth m: "))
-
+    
     M =  (t + n * t + 1 + 1 << (m+1)) * w
 
     if input("printer mode(Y/n): " ).casefold() == "y":
         hash_f = printer
     else:
         hash_f = sha256H
-
 
     print("\nParameters: ")
     print(f"{n=}")
@@ -297,19 +325,19 @@ def main():
     print(f"{m=}")
     print(f"{M=}\n")
 
-    tree = posw(n, m, chi, hash_f)    
+    tree = posw(chi, n, m, hash_f)    
 
     print("Tree: " + mnel.dumps(tree, indent=2))
     challenge = generate_challenge(N, n, t)
-
-    print(list(map(bin, challenge)))
+    #challenge = [3]
+    print(f"challenge: {list(map(bin, challenge))}")
     #print(f"{challenge=}")
     
 
-    reply = open_nodes(tree, challenge, n, m)
+    reply = open_nodes(chi, n, m, tree, challenge, hash_f)
     print(f"{reply=}")
 
-    result = verify(chi, n, tree[""], challenge, reply, hash_f)
+    result = verify(chi, n, tree[ROOT], challenge, reply, hash_f)
     
     if (result):
         print("Verify succeeded")
